9fda003879dd3d464f9c85e8ecfee105fc9d3ea4
migue angel <plusangel07@gmail.com>
2025-07-08 15:51:18 +0200

    feat: Estructura de Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Card.css'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
+    <div
+        className='cardCats'
+        style={{backgroundColor: props.color}}>
+        <img src={props.url} alt={props.imgRota} />
+        <h2>
+            {props.id}
+        </h2>
+        <div className='h3'>
+            <h3>
+                Raza: {props.name}
+            </h3>
+            <h3>
+                Personalidad: {props.temperament}
+            </h3>
+        </div>
+        <p>
+            <strong>Un poco sobre {props.id}</strong>: {props.description}
+        </p>
     </div>
   )
 }
