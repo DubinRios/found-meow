6243df524af2dc03bcf84146cbb72e01e0aeecd3
DubinRios <dubinrioseliecerr@gmail.com>
2025-07-09 15:16:58 +0200

    feat: creación del Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
+  const handleSearch = (query) => {
+    console.log("Buscando:", query);
+  };
+
   return (
     <div className="App">
       <Header />
+      <SearchBar onSearch={handleSearch}/>
     </div>
   );
 };
