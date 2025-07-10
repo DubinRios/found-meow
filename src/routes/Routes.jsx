102d0b83a9fe5dac26e423fb33a22c3849714acc
Yelyzaveta <lizakrasyuk24@gmail.com>
2025-07-10 14:23:07 +0200

    Añadir routes DOM

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route, createBrowserRouter } from "react-router-dom";
+import CatsSlider from '../components/CatsSlider/CatsSlider'
 import Layout from '../layouts/Layout';
 import Home from '../pages/Home';
 import Contact from '../pages/Contact'; 
