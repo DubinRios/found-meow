1e814a460aaa89cbd1308d9dc936cc44f6e403eb
DubinRios <dubinrioseliecerr@gmail.com>
2025-07-11 12:04:42 +0200

    feat: creación del menu hamburguesa en un componente

---
+++ src/components/HamburguerMenu/Menu.jsx
@@ -0,0 +1,34 @@
+import './Menu.css';
+import { Link } from 'react-router-dom';
+
+
+const Menu = ({isOpen, toggleMenu}) => { 
+     
+    
+  return (
+    <div>
+    <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className={`menu-icon ${isOpen ? 'open' : ''}`}>
+            <div className="circle"></div>
+            <div className="lines">
+              <div className="line line1"></div>
+              <div className="line line2"></div>
+              <div className="line line3"></div>
+            </div>
+          </div>
+        
+      </div>
+
+      {isOpen && (
+        <div className="menu-buttons">
+          <Link to="/" className="menu-button">Inicio</Link>
+          <a className="menu-button">Página en Construcción</a>
+          <a className="menu-button">Favoritos ♡</a>
+        </div>
+        
+      )}
+      </div>
+  )
+}
+
+export default Menu
