2e2d674283675883494ed9d386c8ff8e2bcd4713
DubinRios <dubinrioseliecerr@gmail.com>
2025-07-15 11:01:46 +0200

    feat: agregué la card a la página favoritos

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Favorites.css'
+import Card from '../Card/Card'
 
 const Favorites = () => {
   return (
-    <div className='titleF'>
-        
+    <div className='cards-wrapper'>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
     </div>
   )
 }
